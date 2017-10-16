32bcf6863767e09f57e9e4973a0f1cc764146f99
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2017-10-15 21:06:17 -0400

    Milestone 1 released

---
+++ MS1/Date.cpp
@@ -0,0 +1,30 @@
+// Final Project Milestone 1
+//
+// Version 1.0
+// Date
+// Author
+// Description
+//
+//
+//
+//
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+/////////////////////////////////////////////////////////////////
+#include "Date.h"
+
+namespace sict {
+
+  // number of days in month mon_ and year year_
+  //
+  int Date::mdays(int mon, int year)const {
+    int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+    int month = mon >= 1 && mon <= 12 ? mon : 13;
+    month--;
+    return days[month] + int((month == 1)*((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+}
+
+
+
+}
---
+++ MS1/Date.h
@@ -0,0 +1,34 @@
+// Final Project Milestone 1
+//
+// Version 1.0
+// Date
+// Author
+// Description
+//
+//
+//
+//
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+/////////////////////////////////////////////////////////////////
+#ifndef SICT_DATE_H
+#define SICT_DATE_H
+
+namespace sict {
+
+  class Date {
+
+
+     int mdays(int, int)const;
+ 
+  public:
+ 
+  };
+
+
+
+
+
+}
+#endif
